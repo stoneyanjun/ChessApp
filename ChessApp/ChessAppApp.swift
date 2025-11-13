0961352b77a05a50fe3863501fbfdbcc722f56fe
stone <stoneyanjun@gmail.com>
2025-11-13 13:44:47 +0800

    Init project

--- ChessApp/ChessAppApp.swift
+++ ChessApp/ChessAppApp.swift
@@ -2,25 +2,16 @@
 //  ChessAppApp.swift
 //  ChessApp
 //
-//  Created by stone on 2025/11/4.
+//  Created by stone on 2025/11/13.
 //
 
 import SwiftUI
-import CoreData
-import ComposableArchitecture
 
 @main
 struct ChessAppApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
-            GameView(
-                store: Store(
-                    initialState: GameState(),
-                    reducer: { GameFeature() }
-                )
-            )
+            ContentView()
         }
     }
 }
